0b1889412e1c6234ce9a8503f2dd1bd5c13d836b
mohammad hossein sangestani <mhsangestani@gmail.com>
2024-06-28 16:03:58 +0330

    finish the basic cv website

--- website/views.py
+++ website/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_view(request):
-    return render(request, 'website/index.html')
+    context = {
+        'name': 'Mohammad Hossein Sangestani',
+        'skill': 'Django Developer',
+        'about_me': 'I am a mechatronics engineering student, I am interested in website development using Django framework'
+    }
+    return render(request, 'website/index.html',context)
 
 
 def contact_view(request):
@@ -11,8 +16,16 @@ def contact_view(request):
 
 
 def portfolio_view(request):
-    return render(request, 'website\portfolio.html')
+    context = {
+        'project_name': 'tour guide website'
+    }
+    return render(request, 'website\portfolio.html', context)
 
 
 def resume_view(request):
-    return render(request, 'website/resume.html')
+    context = {
+        'field_of_study': 'Mechatronics',
+        'date': '2022 - 2024',
+        'experience': 'I had the opportunity to study computer mechanics and electricity'
+    }
+    return render(request, 'website/resume.html', context)
